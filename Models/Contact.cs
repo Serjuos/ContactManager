a1ac556a046dfde9761fa0884cb7f213e8380dbf
сережа кобяков <sergey.kobyakov.02@mail.ru>
2023-04-12 01:25:46 +0700

    Добавьте файлы проекта.

---
+++ Models/Contact.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    #region snippet1
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        // user ID from AspNetUser table.
+        public string? OwnerID { get; set; }
+
+        [DisplayName("Имя")]
+        public string? Name { get; set; }
+        [DisplayName("Адрес")]
+        public string? Address { get; set; }
+        [DisplayName("Город")]
+        public string? City { get; set; }
+        [DisplayName("Улица")]
+        public string? State { get; set; }
+        [DisplayName("Почтовый индекс")]
+        public string? Zip { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Майл")]
+        public string? Email { get; set; }
+        [DisplayName("Статус")]
+        public ContactStatus Status { get; set; }
+    }
+
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
+    }
+    #endregion
+}
